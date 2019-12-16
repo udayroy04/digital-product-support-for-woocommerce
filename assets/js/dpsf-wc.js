a41cae36c1423d6bffe890b44aa5b4821b095b93
Daffodil Technologies I Pvt Ltd <48059866+udayroy04@users.noreply.github.com>
2019-12-16 18:07:44 +0530

    Extra custom field scripts

--- assets/js/dpsf-wc.js
+++ assets/js/dpsf-wc.js
@@ -1,26 +1,26 @@
 jQuery( document ).ready(function() {
 	
 	
-	jQuery('#add-row').on('click', function() {
-					var row = jQuery('.empty-row.screen-reader-text').clone(true);
-					row.removeClass('empty-row screen-reader-text');
-					row.insertBefore('#repeatable-fieldset-one tbody>tr:last');
+	jQuery("#add-row").on("click", function() {
+					var row = jQuery(".empty-row.screen-reader-text").clone(true);
+					row.removeClass("empty-row screen-reader-text");
+					row.insertBefore("#repeatable-fieldset-one tbody>tr:last");
 					return false;
 				});
-				jQuery('.remove-row').on('click', function() {
-					jQuery(this).parents('tr').remove();
+				jQuery(".remove-row").on("click", function() {
+					jQuery(this).parents("tr").remove();
 					return false;
 	});
 		
 
-           jQuery('#add-supportrow').on('click', function() {
-					var row = jQuery('.empty-supportrow.screen-reader-text').clone(true);
-					row.removeClass('empty-supportrow screen-reader-text');
-					row.insertBefore('#repeatable-support-field tbody>tr:last');
+           jQuery("#add-supportrow").on("click", function() {
+					var row = jQuery(".empty-supportrow.screen-reader-text").clone(true);
+					row.removeClass("empty-supportrow screen-reader-text");
+					row.insertBefore("#repeatable-support-field tbody>tr:last");
 					return false;
 				});
-				jQuery('.remove-supportrow').on('click', function() {
-					jQuery(this).parents('tr').remove();
+				jQuery(".remove-supportrow").on("click", function() {
+					jQuery(this).parents("tr").remove();
 					return false;
 	});
 		
@@ -43,7 +43,7 @@ jQuery( document ).ready(function() {
         });
         
         
-	  jQuery('.upsell-product-title').keydown(function (e) {
+	  jQuery(".upsell-product-title").keydown(function (e) {
 	          if (e.shiftKey || e.ctrlKey || e.altKey) {
 		    jQuery("#upsell-proerrmsg").html("Enter alphabets only").show().fadeOut(10000);
 		     return false;
